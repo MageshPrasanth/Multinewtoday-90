6b3a977b5655dc135d3267857bfe7a72650c0623
MageshPrasanth <mageshprasanth850@gmail.com>
2025-12-18 17:07:36 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "aws-terraform-state-bucket-eks"
+    bucket         = "aws-terraform-state-bucket-eks1"
     key            = "multi-orchestrator/terraform.tfstate"
     region         = "us-east-2"
     encrypt        =  true
